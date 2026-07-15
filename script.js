d37e9fb7ccc94dcbbdfa0078ae6b05746806923d
Noor Fatima <noor2239064@gmail.com>
2026-07-15 17:20:08 +0500

    created the frontend for to do

---
+++ script.js
@@ -0,0 +1,9 @@
+let enterTask = document.querySelector('write-task');
+let submitBtn = document.querySelector('submit-task');
+let taskField =  document.querySelector('task-field');
+let taskDone = document.querySelector('task-done');
+
+function getTask(e){
+    let task = e;
+    console.log(task);
+}
