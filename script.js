469bfa7826664d0db9360f8d0865de420f40d63a
Noor Fatima <noor2239064@gmail.com>
2026-07-18 17:55:06 +0500

    finished the to do app

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let submitTask = document.querySelector('.submit-task');
 let taskContainer = document.querySelector('.task-container');
 let taskField = document.querySelector('.task-field');
 let taskDone = document.querySelector('.task-done');
+let taskAppend = document.querySelector('.task-append')
 
 // to get value from an input field, we target it's value. \
 
@@ -15,7 +16,45 @@ function getTask(){
     else{
     console.log(inputVal);
     writeTask.value = "";
-    taskContainer.classList.remove('invisible');
+
+    const taskWrapper = document.createElement('div');
+    taskWrapper.style.cssText = `
+    background-color: white;      
+    border-radius: 9999px;       
+    padding: 4px;                
+    width: fit-content;           
+    overflow-wrap: break-word;    
+    word-break: break-all;       
+    outline: none;   
+    display:flex;    
+    align-items: center;    
+    `;
+    
+
+    const taskFieldWrapper = document.createElement('p');
+    taskFieldWrapper.style.cssText = `
+    background-color: #ffffff;  
+    border-radius: 9999px;       
+    padding: 2px 10px;         
+    width: fit-content;          
+    word-break: break-all;       
+    overflow-wrap: break-word;  
+    `;
+
+    const icon = document.createElement('i');
+    icon.className = 
+    "fa-solid fa-check bg-green-600 text-white rounded-full text-[16px] p-2 hover:bg-green-700 cursor-pointer h-8 w-8"
+
+    taskWrapper.appendChild(taskFieldWrapper);
+    taskWrapper.appendChild(icon);
+    taskAppend.appendChild(taskWrapper);
+
+    taskFieldWrapper.innerText = inputVal;
+
+    icon.addEventListener('click', ()=>{
+        taskWrapper.remove();
+        alert('Task Removed');
+    })
     }
 }
 
