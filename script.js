86f019e47c41a58fe5c21a719516de8a268406f3
Noor Fatima <noor2239064@gmail.com>
2026-07-18 15:30:46 +0500

    added get logic

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let writeTask = document.querySelector('.write-task');
 let submitTask = document.querySelector('.submit-task');
+let taskContainer = document.querySelector('.task-container');
+let taskField = document.querySelector('.task-field');
+let taskDone = document.querySelector('.task-done');
 
 // to get value from an input field, we target it's value. \
 
 function getTask(){
-    
+    let inputVal = writeTask.value;
+
     if(inputVal===''){
         alert('Please Enter Task');
     }
     else{
-        let inputVal = writeTask.value;
     console.log(inputVal);
+    writeTask.value = "";
+    taskContainer.classList.remove('invisible');
     }
 }
+
+
 
